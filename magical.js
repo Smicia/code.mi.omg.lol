18ec122112de6f5555352611a487043fda833af6
Smicia <76980844+Smicia@users.noreply.github.com>
2022-12-26 18:19:42 +0000

    Create magical.js

---
+++ magical.js
@@ -0,0 +1,18 @@
+//JS for making action only occur IF another script is present. OpenAI'd
+
+const head = document.head;
+const snowstormScript = document.createElement("script");
+snowstormScript.src = "https://js.cache.lol/snowstorm.js";
+
+if (head.contains(snowstormScript)) {
+  const soz = document.getElementById("soz");
+  const snowsoz = document.getElementById("snowsoz");
+
+  if (window.innerWidth >= 600) {
+    soz.style.display = "block";
+    snowsoz.style.display = "none";
+  } else {
+    soz.style.display = "none";
+    snowsoz.style.display = "block";
+  }
+}
