a10818e96f4cc1fd401f734531249e1db4084fc8
Smicia <76980844+Smicia@users.noreply.github.com>
2022-12-27 00:43:40 +0000

    Update footer.php
    Update

--- docs/footer.php
+++ docs/footer.php
@@ -1,7 +1,2 @@
 <!-- Footer -->
-  <footer>
-    <p>
-      Issues? <bold>No support offered</bold>, but feel free to query at mi at omg doot lol.
-    </p>
-    <p style="font-size: medium">Made possible by <a href="https://github.com">GitHub</a> and <a href="https://home.omg.lol/referred-by/mi">omg.lol</a></p>
-  </footer>
+ echo "<p>Test/p>";
