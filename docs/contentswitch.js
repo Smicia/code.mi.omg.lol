66670ca57db4b2bbf7a01c35f8053ad28cd127be
Michael Heffernan <michaelandmax1@gmail.com>
2022-12-30 03:56:41 +0000

    French links

---
+++ docs/contentswitch.js
@@ -0,0 +1,11 @@
+
+/*Veggie Section*/
+function chooseEnglish() {
+	document.getElementsByClassName("french").style.display = "block";
+    document.getElementsByClassName("english").style.display = "none";
+}
+
+function choisissezFrancais() {
+	document.getElementsByClassName("french").style.display = "none";
+    document.getElementsByClassName("english").style.display = "block";
+}
